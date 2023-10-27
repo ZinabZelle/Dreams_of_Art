fd4c2355c9503dc321bba807467a4111f509a301
ZinabZelle <zinab.bidy@gmail.com>
2023-10-27 13:51:12 +0200

    main

--- main/Artwork.py
+++ main/Artwork.py
@@ -6,26 +6,17 @@ class Artwork:
         
 
     def get_info(self):
-        return {
-            "Title": self.title,
-            "Artist": self.artist,
-            "Year": self.year
-        
+        artwork_info = {
+            "Title" : self.title,
+            "Artist" : self.artist,
+            "Year" : self.year
         }
-
-    def update_info(self, new_title, new_artist, new_year):
-        self.title = new_title
-        self.artist = new_artist
-        self.year = new_year
-
-    def get_age(self, current_year):
-        return current_year - self.year
-
-    def is_contemporary(self, current_year, threshold): #a Verifier
-        return self.year >= threshold
+        return artwork_info
 
     def display_info(self):
-        print(f"Title: {self.title}")
-        print(f"Artist: {self.artist}")
-        print(f"Year: {self.year}")
+        info = self.get_info()
+        for key, value in info.items():
+            print(f"{key}: {value}")
+       
+
         
