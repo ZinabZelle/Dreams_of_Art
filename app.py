c7570c5b466099d159f684bfdcebbc4d36031172
ZinabZelle <zinab.bidy@gmail.com>
2023-11-20 16:16:58 +0100

    Add app.py to flask-api branch

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+import json
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def hello():
-    return 'Hello, Dreams of Art!'
+    return 'Hello, Flask API!'
+
+@app.route('/hello',methods=['GET'])
+def hello_dreams_of_art():
+    message = {"message": "Hello Dreams of Art"}
+    return jsonify(message)
 
 if __name__ == '__main__':
     app.run(port=8888)
+    
     
